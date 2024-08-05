0299e983ad3b3ce325d45e71f4c3c75a68fcbee4
nidhik3 <nidhikatkani3.com>
2024-08-05 18:19:10 +0530

    first commiT

---
+++ application/views/main.php
@@ -0,0 +1,227 @@
+<!-- ======= Hero Section ======= -->
+<section>
+  <div class="image-container">
+    <img src="assets/img/banner1.png" alt="Banner Image" class="main-banner">
+    <div class="banner-txt">
+      <p class="bannerH1 mb-0">Driving Growth </p>
+      <p class="bannerH1 md-mt-0 line2">Through Strategic Investments</p>
+      <p class="bannerp ">Creating sustainable value through innovation and expertise.</p>
+      <a class="wTop bannerH2 " href="">Discover More</a>
+    </div>
+  </div>
+</section>
+
+
+<main id="main">
+
+  <!-- ======= About Section ======= -->
+  <section class="no-top about">
+    <div class="container about1 py-4" style="color: #1A406D;">
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-5 space">
+          <h5 class="size1">About</h5>
+          <h5 class="size2 pt-4 pb-5">EIX Investment is a dynamic investment firm based in the UAE, focused on creating
+            value through strategic investments in diverse sectors. Our expertise lies in identifying high-potential
+            opportunities and leveraging our deep industry knowledge to maximize returns for our stakeholders.</h5>
+          <a class="blue1 bannerH2 " href="">Mission & Vision</a>
+        </div>
+      </div>
+    </div>
+    <img src="assets/img/about-banner.png" alt="" style="width: 100%;height: 100%;" class="section-image">
+  </section>
+  <!-- End About Section -->
+
+  <!-- ======= Why us ======= -->
+  <section class="space section-bg">
+    <div class="container">
+      <div class="us">
+        <h5 class="size1">Why Choose Us?</h5>
+        <h5 class="sub1">Our strengths</h5>
+      </div>
+      <div class="container">
+        <div class="row space d-flex justify-content-between">
+          <div class="col-md-5 pt-4 p-md-0 d-flex justify-content-end">
+            <div class="col-md-2 text-center">
+              <img src="assets/img/icons/1.png" alt="">
+            </div>
+            <div class="col-md-5">
+              <h5 class=" u1 mb-0">Strategic Expertise</h5>
+              <p class="g">Leveraging deep industry knowledge to identify high-potential opportunities.</p>
+            </div>
+          </div>
+          <div class="col-md-6 pt-4 p-md-0 d-flex ">
+            <div class="col-md-2 text-center">
+              <img src="assets/img/icons/2.png" alt="">
+            </div>
+            <div class="col-md-4">
+              <h5 class=" u1 mb-0">Innovative Approach</h5>
+              <p class="g">Employing creative strategies to maximize returns.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row no-top d-flex justify-content-between">
+          <div class="col-md-5 pt-4 p-md-0 d-flex justify-content-end">
+            <div class="col-md-2 text-center">
+              <img src="assets/img/icons/3.png" alt="">
+            </div>
+            <div class="col-md-5">
+              <h5 class=" u1 mb-0">Client-Centric</h5>
+              <p class="g">Building long-term relationships based on trust and transparency.</p>
+            </div>
+          </div>
+          <div class="col-md-6  pt-4 p-md-0  d-flex ">
+            <div class="col-md-2 text-center">
+              <img src="assets/img/icons/4.png" alt="">
+            </div>
+            <div class="col-md-5">
+              <h5 class=" u1 mb-0">Sustainable Impact</h5>
+              <p class="g">Committing to responsible investments that positively impact society and the environment.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Why us-->
+
+
+
+  <!-- ======= Career Section ======= -->
+  <section class="space">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="assets/img/career.png" alt="" style=" height: 100%;
+    width: 95%;">
+        </div>
+        <div class="col-md-5 space">
+          <h5 class="size1">Careers</h5>
+          <p class="g">Join Our Team</p>
+          <h5 class="pt-4 pb-5">We are looking for talented individuals to join our innovative team. Explore current job
+            openings and become a part of EIX Investment.</h5>
+          <a class="blue1 " href="">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Career Section -->
+
+  <!-- ======= Testimonials Section ======= -->
+  <section id="testimonials" class="testimonials">
+    <div class="container position-relative">
+
+      <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+              <h3>Saul Goodman</h3>
+              <h4>Ceo &amp; Founder</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium
+                quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+              <h3>Sara Wilsson</h3>
+              <h4>Designer</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis
+                quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+              <h3>Jena Karlis</h3>
+              <h4>Store Owner</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor
+                labore quem eram duis noster aute amet eram fore quis sint minim.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
+              <h3>Matt Brandon</h3>
+              <h4>Freelancer</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim
+                dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
+              <h3>John Larson</h3>
+              <h4>Entrepreneur</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa
+                labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+          </div><!-- End testimonial item -->
+
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+
+    </div>
+  </section><!-- End Testimonials Section -->
+
+  <!-- ======= insight Section ======= -->
+  <section class="space">
+<div class="container">
+    <h5 class="size1">Insights</h5>
+    <h5 class="sub1">News</h5>
+    <div class="row  d-flex ">
+      <div class="col-md-5">
+        <img src="assets/img/insight.png" alt="" class="py-5">
+        <a class="blue1 " href="">Read More</a>
+      </div>
+      <div class="col-md-1">
+        
+      </div>
+      <div class="col-md-5">
+        <img src="assets/img/insight.png" alt="" class="py-5">
+        <a class="blue1 " href="">Read More</a>
+      </div>
+    
+    </div>
+</div>
+  </section>
+  <!-- End insight Section -->
+
+
+</main><!-- End #main -->
